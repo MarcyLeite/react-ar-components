228700251aac17ba7081864313a5d31c9e80d0df
Matheus Marciano <mm.leitea@outlook.com>
2024-10-31 14:04:46 -0300

    🚚 Rearrange scss composition files

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import './assets/reset.scss';
-import './assets/palette.scss';
-import './assets/composition.scss'
-import './assets/index.scss';
+import './assets/css/index.scss'
 
-import App from './App';
+import App from './App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
